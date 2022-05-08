76206bb8a63d7411469e78a303e700e412c939ea
Bryana Aleman <alemanb2018@gmail.com>
2022-05-08 07:55:53 -0500

    updated ip deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.208.13.51',
+      host: '23.22.244.234',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
